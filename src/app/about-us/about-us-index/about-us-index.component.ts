362ed819780ff27fb298f82d662ea5774c18d169
Subhra Sekhar Mukherjee <subhra.php@gmail.com>
2019-07-22 10:40:20 +0530

    PWA is working

--- src/app/about-us/about-us-index/about-us-index.component.ts
+++ src/app/about-us/about-us-index/about-us-index.component.ts
@@ -7,14 +7,15 @@ import { ApiService } from 'src/app/services/api.service';
   styleUrls: ['./about-us-index.component.scss']
 })
 
+
 export class AboutUsIndexComponent implements OnInit {
 
-  theJoke : String = 'Joke is loading'
+  theJoke: String;
 
   constructor(private api: ApiService) { }
 
   ngOnInit() {
-    this.api.gemmyJokes().subscribe((result : any )  => {
+    this.api.gemmyJokes().subscribe((result: any) => {
       this.theJoke = result.value.joke
     })
   }
